870445350ef77d6e2160d58fda34f75b0fa37599
Mukul Saini <138042294+mukulsainisbl@users.noreply.github.com>
2024-05-19 14:05:04 +0530

    States

---
+++ src/components/TaskManager.jsx
@@ -0,0 +1,49 @@
+// src/components/TaskManager.jsx
+
+import { useState } from "react";
+
+function TaskManager() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
+  const removeTask = () => {
+    setTasks(tasks.slice(1));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add Task</button>
+      <button
+        onClick={() => {
+          /*complete the missing code*/
+         setTasks(tasks.slice(1))
+        }}
+      >
+        Remove First Task
+      </button>
+      <ul>
+        {tasks.map((task, index) => (
+          <> <li key={index}>{task} <button onClick={removeTask}>Delete</button></li>
+
+
+          </>
+         
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TaskManager;
